b717b67dc8cddd328d507793875cd4f5b19cc8c2
Robert Jomar Malate <robjmal@gmail.com>
2026-07-29 13:53:59 +0200

    feat: completed the bigram base

--- src/model.py
+++ src/model.py
@@ -14,9 +14,40 @@ class BigramLanguageModel(nn.Module):
         # Each token directly readds of the lgits for next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
-    def forward(self, idx, targets):
+    def forward(self, idx, targets = None):
         # idx and targets are both (B, T) tensor of integers
         logits = self.token_embedding_table(idx)    # (Batch, Time, Channel)
 
+        # This is for some particular shape
+        if targets is None:
+            loss = None
+        else: 
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+
+            # Pytorch cross_entropy wants (B,C,T)
+            loss = F.cross_entropy(logits, targets) # -log likelihood of cross and targets
+
         # Scores for next character in the sequence 
-        return logits
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+
+            # focus on last time step
+            logits = logits[:, -1, :]   # Becomes (B, C)
+
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)   # (B, C)
+
+            # sample from distribution, just 1 sample
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+
+            # append sampled indx to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+
+        return idx
